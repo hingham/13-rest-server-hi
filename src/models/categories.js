91ddf1b07d022724f7de96681759a4e0d81dc521
hingham <hannah.c.ingham@gmail.com>
2019-01-09 18:49:37 -0800

    adding category stuff

--- src/models/categories.js
+++ src/models/categories.js
@@ -1,22 +1,48 @@
 'use strict';
+let sanitize = require('./sanitize');
+let uuid = require('uuid/v4');
 
 class Categories {
 
   constructor() {
+    this.db = [];
   }
 
   get(_id) {
+    let response = _id ? this.db.filter( record => record._id === _id) : this.db;
+    return Promise.resolve(response);
   }
   
-  post(record) {
+  post(entry) {
+    entry._id = uuid();
+    let record = sanitize(entry);
+    if(record._id){this.db.push(record);}
+    return Promise.resolve(record);
   }
 
-  put(_id, record) {
+  put(_id, entry) {
+    entry._id = _id;
+    let record = sanitize(entry);
+    for (let i = 0; i< this.db.length; i++){
+      if(this.db[i]._id === _id){
+        this.db[i] = record;
+      }
+    }
+    return Promise.resolve(record);
   }
 
   delete(_id) {
+    let deleted;
+    for (let i = 0; i< this.db.length; i++){
+      if(this.db[i]._id === _id){
+        deleted = this.db[i];
+        this.db.splice(i, 1);
+      }
+    }
+    return Promise.resolve(deleted);
   }
 
 }
+
 
 module.exports = Categories;
